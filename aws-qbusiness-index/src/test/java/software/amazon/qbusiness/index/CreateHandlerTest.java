4eec1dcf186cbd04f93f0e9f0034d3675ef793bf
Ka Ho Chan <chnmka@amazon.com>
2024-04-10 19:52:11 +0000

    [IndexType] Update resource json and handler
    cr: https://code.amazon.com/reviews/CR-123494830

--- aws-qbusiness-index/src/test/java/software/amazon/qbusiness/index/CreateHandlerTest.java
+++ aws-qbusiness-index/src/test/java/software/amazon/qbusiness/index/CreateHandlerTest.java
@@ -97,7 +97,7 @@ public class CreateHandlerTest extends AbstractTestBase {
         .description("A Description")
         .applicationId(APP_ID)
         .capacityConfiguration(new IndexCapacityConfiguration(10D))
-        .type(IndexType.HA.toString())
+        .type(IndexType.ENTERPRISE.toString())
         .build();
 
     testRequest = ResourceHandlerRequest.<ResourceModel>builder()
@@ -134,7 +134,7 @@ public class CreateHandlerTest extends AbstractTestBase {
             .createdAt(Instant.ofEpochMilli(1697824935000L))
             .updatedAt(Instant.ofEpochMilli(1697839335000L))
             .status(IndexStatus.ACTIVE)
-            .type(IndexType.HA)
+            .type(IndexType.ENTERPRISE)
             .description(createModel.getDescription())
             .displayName(createModel.getDisplayName())
             .capacityConfiguration(software.amazon.awssdk.services.qbusiness.model.IndexCapacityConfiguration.builder()
@@ -183,7 +183,7 @@ public class CreateHandlerTest extends AbstractTestBase {
     var statusResponseBuilder = GetIndexResponse.builder()
         .applicationId(APP_ID)
         .indexId(INDEX_ID)
-        .type(IndexType.HA)
+        .type(IndexType.ENTERPRISE)
         .createdAt(Instant.ofEpochMilli(1697824935000L))
         .updatedAt(Instant.ofEpochMilli(1697839335000L))
         .status(IndexStatus.ACTIVE);
@@ -194,7 +194,7 @@ public class CreateHandlerTest extends AbstractTestBase {
         .thenReturn(statusResponseBuilder.status(IndexStatus.ACTIVE).build())
         .thenReturn(statusResponseBuilder
             .status(IndexStatus.ACTIVE)
-            .type(IndexType.HA)
+            .type(IndexType.ENTERPRISE)
             .description(createModel.getDescription())
             .displayName(createModel.getDisplayName())
             .capacityConfiguration(software.amazon.awssdk.services.qbusiness.model.IndexCapacityConfiguration.builder()
@@ -233,7 +233,7 @@ public class CreateHandlerTest extends AbstractTestBase {
     var getResponse = GetIndexResponse.builder()
         .applicationId(APP_ID)
         .indexId(INDEX_ID)
-        .type(IndexType.HA)
+        .type(IndexType.ENTERPRISE)
         .createdAt(Instant.ofEpochMilli(1697824935000L))
         .updatedAt(Instant.ofEpochMilli(1697839335000L))
         .description(createModel.getDescription())
@@ -285,7 +285,7 @@ public class CreateHandlerTest extends AbstractTestBase {
         .thenReturn(GetIndexResponse.builder()
             .applicationId(APP_ID)
             .indexId(INDEX_ID)
-            .type(IndexType.HA)
+            .type(IndexType.ENTERPRISE)
             .createdAt(Instant.ofEpochMilli(1697824935000L))
             .updatedAt(Instant.ofEpochMilli(1697839335000L))
             .status(IndexStatus.FAILED)
